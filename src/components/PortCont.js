7284a1543c076d2f0e775a0f96ff78d459ae52a1
kaozidd <arthur_hemingway@outlook.com>
2018-03-13 16:33:52 -0600

    update

--- src/components/PortCont.js
+++ src/components/PortCont.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Header from './Header';
+import Summary from './Summary';
+import ContactInfo from './ContactInfo';
 
 class PortCont extends Component {
 	render(){
@@ -8,54 +11,6 @@ class PortCont extends Component {
 				<Summary />
 				<ContactInfo />
 			</div>
-		);
-	}
-}
-
-class Header extends Component {
-	render(){
-		return (
-			<header>
-				<h2>Portfolio</h2>
-			</header>
-		);
-	}
-}
-
-class Summary extends Component {
-	render(){
-		return (
-			<section>
-	          <h4>Summary</h4>
-	          <p>I am a designer and full stack developer with a primary focus on the front-end. I have worked on all layers of a project from implementing designer mockups, custom animations, and building APIs.</p>
-	        </section>
-		);
-	}
-}
-
-class ContactInfo extends Component {
-	render(){
-		return (
-			<section>
-	          <div className="contactinfo">
-	            <a className="contactinfo__single" target="_blank">
-	              <i className="ion-ios-location-outline contactinfo__icon"></i>
-	              <span className="contactinfo__title">Austin, TX USA</span>
-	            </a>
-	            <a href="#" className="contactinfo__single" target="_blank">
-	              <i className="ion-ios-email-outline contactinfo__icon"></i>
-	              <span className="contactinfo__title">mariasantiago1989@gmail.com</span>
-	            </a>
-	            <a href="#" className="contactinfo__single" target="_blank">
-	              <i className="ion-social-github-outline contactinfo__icon"></i>
-	              <span className="contactinfo__title">devmaria</span>
-	            </a>
-	            <a href="#" className="contactinfo__single" target="_blank">
-	              <i className="ion-social-linkedin-outline contactinfo__icon"></i>
-	              <span className="contactinfo__title">maria-x-santi</span>
-	            </a>
-	          </div>
-	        </section>
 		);
 	}
 }
